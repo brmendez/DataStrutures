b4901a2a47f04633a4e862fc7dc25e9ea3aaaee6
Brian Mendez <brmendez@gmail.com>
2014-12-07 15:30:29 -0800

    Implemented Linked List

--- LinkedList.playground/section-1.swift
+++ LinkedList.playground/section-1.swift
@@ -2,4 +2,23 @@
 
 import UIKit
 
-var str = "Hello, playground"
+class Node {
+    var value : String?
+    var next : Node?
+}
+
+class LinkedList {
+    var head = Node()
+    
+    func insert(value: String){
+
+    if head == nil {
+        var node = Node()
+        node.value = value
+        node.next = nil
+        head = node
+    }
+    
+    }
+}
+
--- Stack.playground/section-1.swift
+++ Stack.playground/section-1.swift
@@ -14,6 +14,5 @@ class Stack {
         itemArray.removeLast()
         return popItem!
     }
-    
-    
+
 }
